32a86353d039786708cc58101a4ea92c5d7d7f86
摇摇猪 <73538420+YaoYao-Pig@users.noreply.github.com>
2025-11-29 20:03:56 +0800

    feat: 修改配置

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MyBlog/', // <--- 这里的名字必须和你的 GitHub 仓库名完全一致，前后都要有斜杠！
 })
